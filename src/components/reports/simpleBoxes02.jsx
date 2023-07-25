526e52d9ba5b8b71a6aedbf69ae59f710a141933
Mateo Cao <mateocao12@gmail.com>
2023-07-25 00:39:15 -0300

    Reports Responsive

--- src/components/reports/simpleBoxes02.jsx
+++ src/components/reports/simpleBoxes02.jsx
@@ -2,32 +2,32 @@ import boxes from './simpleBoxes02-vars.json'
 
 export const simpleBoxes02 = () => {
   return (
-    <section id='simpleBoxes02' className='w-full h-96 flex justify-around items-center flex-wrap bg-palette-light-bg300 dark:bg-palette-light-txt700'>
-      { boxMap() }
+    <section id='simpleBoxes02' className='w-full min-h-[420px]  flex flex-row gap-5 justify-around items-center flex-wrap bg-slate-200 dark:bg-slate-700 pt-5'>
+      {boxMap()}
     </section>
   )
 }
 
 function boxMap () {
-  return boxes.map ( (b, key) => Box(b,key) )
+  return boxes.map((b, key) => Box(b, key))
 }
 
 function Box (data, id) {
   return (
     <ul id={'simpleBoxes02-box0' + id} className='w-80 h-52 flex flex-wrap py-1 px-3 shadow-lg rounded-lg border bg-palette-light-bg400 dark:bg-palette-light-txt800'>
       <li id={'simpleBoxes02-box0' + id + '-horizontalUp'} className='w-full h-1/3 justify-evenly items-center flex flex-wrap'>
-        <p id={'simpleBoxes02-box0' + id + '-horizontalUp-text'} className='text-palette-light-txt800 dark:text-palette-light-bg100'>{ data.title } </p>
+        <p id={'simpleBoxes02-box0' + id + '-horizontalUp-text'} className='text-palette-light-txt800 dark:text-palette-light-bg100'>{data.title} </p>
         <div id={'simpleBoxes02-box0' + id + '-imgContainer'} className=''>
           <img id={'simpleBoxes02-box0' + id + '-img'} alt='' />
-          </div>
+        </div>
       </li>
       <li id={'simpleBoxes02-box0' + id + '-horizontalDown'} className='w-full h-2/3 flex flex-wrap items-center justify-evenly'>
         <div id={'simpleBoxes02-box0' + id + '-horizontalDown-left'} className='w-1/2 h-full flex flex-wrap flex-col'>
-          <p id={'simpleBoxes02-box0' + id + '-horizontalDown-left-txt01'} className='text-palette-light-txt800 dark:text-palette-light-bg100'> { data.data01 } </p>
-          <p id={'simpleBoxes02-box0' + id + '-horizontalDown-left-txt02'} className='text-palette-light-txt800 dark:text-palette-light-bg100'> { data.data02 } </p>
+          <p id={'simpleBoxes02-box0' + id + '-horizontalDown-left-txt01'} className='text-palette-light-txt800 dark:text-palette-light-bg100'> {data.data01} </p>
+          <p id={'simpleBoxes02-box0' + id + '-horizontalDown-left-txt02'} className='text-palette-light-txt800 dark:text-palette-light-bg100'> {data.data02} </p>
         </div>
         <div id={'simpleBoxes02-box0' + id + '-horizontalDown-right-imgContainer'} className='w-1/2 h-full flex flex-wrap flex-row'>
-          <img id={'simpleBoxes02-box0' + id + '-horizontalDown-right-img'} alt={ data.img } />
+          <img id={'simpleBoxes02-box0' + id + '-horizontalDown-right-img'} alt={data.img} />
         </div>
       </li>
     </ul>
--- src/components/reports/table01.jsx
+++ src/components/reports/table01.jsx
@@ -2,19 +2,18 @@ import section from './table01-const.json'
 import lsReports from './table01-vars.json'
 
 export const table01 = () => {
-
   return (
-    <section id='table01' className='w-full h-auto flex flex-wrap items-center justify-center py-5 dark:bg-palette-light-txt700 bg-palette-light-bg100'>
-      <ul id='table01--table' className='w-11/12 flex flex-wrap rounded-lg shadow-lg bg-palette-light-bg300 dark:bg-palette-light-txt800'>
+    <section id='table01' className='w-full h-auto flex text-xs py-16 sm:text-base flex-wrap items-center justify-center px-1bg-slate-200 dark:bg-slate-700'>
+      <ul id='table01--table' className='md:w-9/12 flex flex-wrap rounded-lg shadow-lg bg-slate-300 dark:bg-slate-800 border'>
         <li id='table01--titleCols' className='p-1 w-full flex flex-wrap'>
-          <ul id='table01--titleCols--inner'  className='p-1 w-full flex flex-wrap rounded-lg items-center justify-evenly'>
-            <li id='table01-fieldHead-col01' className='w-1/12 flex flex-wrap dark:text-palette-light-bg100 :text-palette-light-txt800'>{section.titleColumn01}</li>
-            <li id='table01-fieldHead-col02' className='w-1/12 flex flex-wrap dark:text-palette-light-bg100 :text-palette-light-txt800'>{section.titleColumn02}</li>
-            <li id='table01-fieldHead-col03' className='w-1/2 flex flex-wrap dark:text-palette-light-bg100 :text-palette-light-txt800'>{section.titleColumn03}</li>
-            <li id='table01-fieldHead-col04' className='w-1/12 flex flex-wrap dark:text-palette-light-bg100 :text-palette-light-txt800'>{section.titleColumn04}</li>
+          <ul id='table01--titleCols--inner' className='p-1 w-full flex flex-wrap rounded-lg items-center justify-evenly'>
+            <li id='table01-fieldHead-col01' className='w-1/12 py-2 flex flex-wrap dark:text-palette-light-bg100 :text-palette-light-txt800'>{section.titleColumn01}</li>
+            <li id='table01-fieldHead-col02' className='w-1/12 py-2 flex flex-wrap dark:text-palette-light-bg100 :text-palette-light-txt800'>{section.titleColumn02}</li>
+            <li id='table01-fieldHead-col03' className='w-1/2 py-2 flex flex-wrap dark:text-palette-light-bg100 :text-palette-light-txt800'>{section.titleColumn03}</li>
+            <li id='table01-fieldHead-col04' className='w-1/12 py-2 flex flex-wrap dark:text-palette-light-bg100 :text-palette-light-txt800'>{section.titleColumn04}</li>
           </ul>
         </li>
-        { rowMap() }
+        {rowMap()}
       </ul>
     </section>
   )
@@ -24,12 +23,12 @@ const rowMap = () => {
   return (
     lsReports.map((report, key) => {
       return (
-        <li key={key} id={'table01--row0' + key}  className='p-1 w-full flex flex-wrap'>
+        <li key={key} id={'table01--row0' + key} className='p-1 w-full flex flex-wrap'>
           <ul id={'table01--row0' + key + '--inner'} className='p-1 w-full flex flex-wrap rounded-lg items-center justify-evenly'>
-            <li id={'table01-row0' + key + '-col01'} className='w-1/12 flex flex-wrap dark:text-palette-light-bg100 :text-palette-light-txt800'>{report.año}</li>
-            <li id={'table01-row0' + key + '-col02'} className='w-1/12 flex flex-wrap dark:text-palette-light-bg100 :text-palette-light-txt800'>{report.periodo}</li>
-            <li id={'table01-row0' + key + '-col03'} className='w-1/2 flex flex-wrap dark:text-palette-light-bg100 :text-palette-light-txt800'>{report.nombre}</li>
-            <li id={'table01-row0' + key + '-col04'} className='w-1/12 flex flex-wrap dark:text-palette-light-bg100 :text-palette-light-txt800'><a href={report.urlInforme}>{section.downloadButton}</a></li>
+            <li id={'table01-row0' + key + '-col01'} className='w-1/12 py-2 flex flex-wrap dark:text-palette-light-bg100 :text-palette-light-txt800'>{report.año}</li>
+            <li id={'table01-row0' + key + '-col02'} className='w-1/12 py-2 flex flex-wrap dark:text-palette-light-bg100 :text-palette-light-txt800'>{report.periodo}</li>
+            <li id={'table01-row0' + key + '-col03'} className='w-1/2 py-2 flex flex-wrap dark:text-palette-light-bg100 :text-palette-light-txt800'>{report.nombre}</li>
+            <li id={'table01-row0' + key + '-col04'} className='w-1/12 py-2 flex flex-wrap dark:text-palette-light-bg100 :text-palette-light-txt800'><a href={report.urlInforme}>{section.downloadButton}</a></li>
           </ul>
         </li>
       )
