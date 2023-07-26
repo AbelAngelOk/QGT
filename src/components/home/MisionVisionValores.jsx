b049232bfcd4361e591704a9674003b33d322eb0
Mateo Cao <mateocao12@gmail.com>
2023-07-26 15:59:12 -0300

    Responsive MisionVisionValores

--- src/components/home/MisionVisionValores.jsx
+++ src/components/home/MisionVisionValores.jsx
@@ -3,7 +3,7 @@ import { Superior, Contenedor } from './mvvSuperior'
 
 export const MisionVisionValores = () => {
   return (
-    <section className='bg-[#282c34] grid grid-cols-[repeat(3,_minmax(0,_33.33%))] grid-rows-[repeat(4,_minmax(0,_13vw))] my-8 w-fit mx-auto'>
+    <section className='h-auto bg-[#282c34] grid grid-cols-1 lg:grid-cols-[repeat(3,_minmax(0,_33.33%))] sm:grid-rows-[repeat(4,_minmax(0,_600px))] grid-rows-[repeat(4,_minmax(0,_650px))] lg:grid-rows-[repeat(4,_minmax(0,_13vw))] my-8 lg:w-fit mx-auto'>
       <Superior />
       {data.map(info => {
         return (
--- src/components/home/mvvSuperior.jsx
+++ src/components/home/mvvSuperior.jsx
@@ -2,13 +2,14 @@ import data from './mvvSuperior.json'
 
 export const Superior = () => {
   return (
-    <div className='col-span-3 flex items-center justify-between shadow-[0_8px_6px_-2px_black]'>
-      <div className='text-center w-[45%] border-r-2 h-3/5'>
-        <h2 className='text-[1.2vw] p-0 mt-11'>{data.title}</h2>
+    <div className='lg:col-span-3 flex flex-col lg:flex-row items-center justify-center lg:justify-between gap-16 lg:gap-0 shadow-[0_8px_6px_-2px_black]'>
+      <div className='text-center lg:h-3/5 w-10/12 lg:w-[48%]  flex items-center justify-center'>
+        <h2 className='text-3xl lg:text-[1.3vw] p-0  text-white font-semibold'>{data.title}</h2>
       </div>
-      <div className='w-1/2 p-3'>
-        <p className='text-[1vw] mb-3'>{data.p1}</p>
-        <p className='text-[1vw] mt-3'>{data.p2}</p>
+      <div className='w-8/12 lg:w-fit text-center border-b-2 lg:border-r-2 lg:border-b-0 lg:h-3/5' />
+      <div className='p-3 lg:w-[48%] w-10/12'>
+        <p className='lg:text-[1vw] mb-3 text-white'>{data.p1}</p>
+        <p className='lg:text-[1vw] mt-3 text-white'>{data.p2}</p>
       </div>
     </div>
   )
@@ -16,16 +17,16 @@ export const Superior = () => {
 
 export const Contenedor = ({ info }) => {
   return (
-    <div className='row-span-3'>
-      <div style={{ backgroundImage: `url('${info.img}')` }} className='bg-cover bg-center absolute w-[33.33%] h-[39vw] opacity-30' />
-      <div className='h-full flex flex-col font-bold w-4/5 mt-10 mx-auto relative right-0  gap-7 lg:gap-10'>
-        <h2 className='text-[1.7vw] lg:text-[1.5vw] text-white'>
+    <div className='row-span-1 lg:row-span-3'>
+      <div style={{ backgroundImage: `url('${info.img}')` }} className='bg-cover bg-center absolute w-full lg:w-[33.33%] min-h-[650px] sm:min-h-[600px] lg:min-h-0 lg:h-[39vw] opacity-30' />
+      <div className='h-full flex flex-col justify-center items-center lg:justify-normal lg:items-stretch font-bold w-4/5 lg:mt-10 mx-auto relative right-0  gap-16 lg:gap-10 py-3'>
+        <h2 className='text-3xl lg:text-[1.5vw] text-white'>
           {info.title}
         </h2>
-        <p className=' text-white font-semibold text-[1.1vw] lg:text-[1vw]'>
+        <p className=' text-white font-semibold lg:text-[1vw] text-center lg:text-left'>
           {info.p1}
         </p>
-        <p className='text-white font-semibold text-[1.1vw] lg:text-[1vw]'>
+        <p className='text-white font-semibold lg:text-[1vw] text-center lg:text-left'>
           {info.p2}
         </p>
       </div>
